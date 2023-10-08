b0df7bd0e4690fa0800c5bf35544fa45bfe1a460
Jose Andres Saavedra Romero <josex.1995@hotmail.com>
2023-10-08 00:23:18 -0500

    feat: modulo shared y modulo gym
    Se crea el modulo shared en donde se agrega el componente footer, de igual manera se agrega el modulo gym; se istala angular material, se instala ngx-spinner, se agregan stilos globales al proyecto como colores y tamaños en 'px'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GymModule } from './gym/gym.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    GymModule,
+    SharedModule
   ],
-  providers: [],
+  providers: [
+    
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/gym/gym.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NgxSpinnerModule } from 'ngx-spinner';
+
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  exports: [
+    NgxSpinnerModule
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class GymModule { }
