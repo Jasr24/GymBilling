80ee449f5d017a14aa5c5dddd786b96ada8517a7
Jose Andres Saavedra Romero <josex.1995@hotmail.com>
2024-06-02 17:08:35 -0500

    Feat: Se a{ade iconos en la tabla, para editar, eliminar y ver.

--- src/app/gym/interfaces/DataBaseResponse.ts
+++ src/app/gym/interfaces/DataBaseResponse.ts
@@ -1,11 +1,17 @@
 export interface ClienteResponseI {
-    id : number
-    nombres : string
-    apellidos : string
-    tipoIdentificacion: number
-    identificacion : string
-    telefono : number
-    email : string
-    estado : number
-    nota : string
+    id : number;
+    nombres : string;
+    apellidos : string;
+    tipoIdentificacion: number;
+    identificacion : string;
+    telefono : number;
+    email : string;
+    estado : number;
+    nota : string;
+    opciones?: {
+        id: number,
+        icon: string,
+        tool_tip: string,
+        cass: string
+    }
 }
--- src/app/gym/pages/estado-cuentas/estado-cuentas.component.ts
+++ src/app/gym/pages/estado-cuentas/estado-cuentas.component.ts
@@ -16,19 +16,20 @@ export class EstadoCuentasComponent extends Base implements OnInit{
   dtList: ClienteResponseI[] = [];
 
   nameColumns = [
-    "Id",
-    "Nombres",
-    "Apellidos",
+    //"Id",
+    "Nombre",
+    "Apellido",
     "Tipo identificacion",
     "Identificación",
     "Teléfono",
     "Email",
     "Estado",
-    "Observaciones"
+    "Observaciones",
+    "Acciones"
   ]
 
   attrData= [
-    "id",
+    //"id",
     "nombres",
     "apellidos",
     "documentos_id",
@@ -36,7 +37,8 @@ export class EstadoCuentasComponent extends Base implements OnInit{
     "telefono",
     "email",
     "estado",
-    "nota"
+    "nota",
+    "opciones"
   ]
 
   constructor(
@@ -58,7 +60,31 @@ export class EstadoCuentasComponent extends Base implements OnInit{
           console.log(r)
           this.spinner.hide();
           if (r.success) {
-            this.dtList = r.data
+            this.dtList = r.data.map((cliente: ClienteResponseI) => {
+              return {
+                ...cliente,
+                opciones: [
+                  {
+                    id: 1,
+                    icon: "visibility",
+                    class: "btn-view",
+                    tool_tip: "Ver",
+                  },
+                  {
+                    id: 2,
+                    icon: "edit",
+                    class: "btn-edit",
+                    tool_tip: "Editar",
+                  },
+                  {
+                    id: 3,
+                    icon: "delete",
+                    class: "btn-delete",
+                    tool_tip: "Eliminar",
+                  }
+                ]
+              };
+            });
           } else {
             this.openDialogs('Error', 'Ha ocurrido un error, por favor inténtelo nuevamente más tarde.', 2);
           }
@@ -72,6 +98,16 @@ export class EstadoCuentasComponent extends Base implements OnInit{
         console.log(err)
       }
     }, 1500);
+  }
+
+  action($event: any){
+    if($event.id == 1){
+      console.log("Ver")
+    } else if ($event.id == 2){
+      console.log("Editar")
+    } else if ($event.id == 3){
+      console.log("Eliminar")
+    }
   }
 
   openDialogs(title: String, message: String, type: number): void {
